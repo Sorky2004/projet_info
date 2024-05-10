b6b1e3dc008c156f5ac614f1c32fdb3bd418ef28
loubna <162711167+qloubna@users.noreply.github.com>
2024-05-10 23:21:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('final3.html')
+    return render_template('TriVisio.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
